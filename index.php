918c4cae46acd4bab7efc2062fb21b364e5e193e
root <root@instance-1.us-central1-a.c.fourth-tiger-237502.internal>
2019-08-25 22:46:20 +0000

    Adicionando Admin Layout

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php 
 
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +18,15 @@ $app->config('debug', true);
 $app->get('/', function() {
     
     $page = new Page();
+
+    $page->setTpl("index");
+    
+
+});
+
+$app->get('/admin', function() {
+    
+    $page = new PageAdmin();
 
     $page->setTpl("index");
     
