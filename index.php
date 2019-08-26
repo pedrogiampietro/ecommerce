f2038cdb6f92673d5018d71272e75bc3b9475f6f
root <root@instance-1.us-central1-a.c.fourth-tiger-237502.internal>
2019-08-26 19:50:05 +0000

    Login e Logout

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
-<?php 
+<?php
 
 error_reporting(-1); // reports all errors
 ini_set("display_errors", "1"); // shows all errors
 ini_set("log_errors", 1);
-ini_set("error_log", "/tmp/php-error.log");
+ini_set("error_log", "/tmp/php-error.log"); 
+
+session_start();
 
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -25,12 +28,45 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
     $page = new PageAdmin();
 
     $page->setTpl("index");
     
 
+});
+
+$app->get('/admin/login', function() {
+    
+    $page = new PageAdmin([
+
+    	"header"=>false,
+    	"footer"=>false
+    ]);
+
+    $page->setTpl("login");
+    
+
+});
+
+$app->post('/admin/login', function() {
+    
+  	User::login($_POST["login"], $_POST["password"]); 
+
+  	header("Location: /admin");
+  	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+	
 });
 
 $app->run();
